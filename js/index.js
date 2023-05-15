81797a1f3b242cc5aec411722fb636b655436053
Javier Grau <javiergrau.freelance@gmail.com>
2023-05-15 19:00:14 -0400

    Add menu 'display' change with 'addEventerListener'

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-let btn = document.querySelector("#hamburger-bet");
+var btn = document.querySelector("#hamburger-btn");
+var hiddenMenuBg = document.querySelector("#hidden-menu-background");
+var hiddenMenu = document.querySelector("#hidden-menu");
 
-
-
-btn.addEventListener("click", menuEvent() {
-
+btn.addEventListener("click", function () {
+    hiddenMenuBg.style.display = "initial";
+    hiddenMenu.style.display = "initial";
+    hiddenMenuBg.style.opacity = 1;
+    hiddenMenu.style.opacity = 1;
 });
