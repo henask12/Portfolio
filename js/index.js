e1e2fa8d69ce6b8491cfe64eae93f3e24a0313fa
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-05-19 18:26:10 +0300

    update the cards version to make dynamic

--- js/index.js
+++ js/index.js
@@ -1,173 +1,123 @@
-const btn = document.querySelector('#hamburger-btn');
-const closeBtn = document.querySelector('#close-btn');
-const hiddenMenuBg = document.querySelector('#hidden-menu-background');
-const hiddenMenu = document.querySelector('#hidden-menu');
-const anchorMenu = document.querySelector('.anchor');
-const anchorMenu1 = document.querySelector('.anchor1');
-const anchorMenu2 = document.querySelector('.anchor2');
+const btn = document.querySelector("#hamburger-btn");
+const closeBtn = document.querySelector("#close-btn");
+const hiddenMenuBg = document.querySelector("#hidden-menu-background");
+const hiddenMenu = document.querySelector("#hidden-menu");
+const anchorMenu = document.querySelector(".anchor");
+const anchorMenu1 = document.querySelector(".anchor1");
+const anchorMenu2 = document.querySelector(".anchor2");
 let isMenuOpen = false;
 
 function openMenu() {
-  hiddenMenuBg.style.display = 'initial';
-  hiddenMenu.style.display = 'initial';
-  setTimeout(() => {
-    hiddenMenuBg.style.opacity = 1;
-    hiddenMenu.style.opacity = 1;
-  }, 10);
-  isMenuOpen = true;
+    hiddenMenuBg.style.display = "initial";
+    hiddenMenu.style.display = "initial";
+    setTimeout(() => {
+        hiddenMenuBg.style.opacity = 1;
+        hiddenMenu.style.opacity = 1;
+    }, 10);
+    isMenuOpen = true;
 }
 
 function closeMenu() {
-  hiddenMenuBg.style.opacity = 0;
-  hiddenMenu.style.opacity = 0;
-  setTimeout(() => {
-    hiddenMenuBg.style.display = 'none';
-    hiddenMenu.style.display = 'none';
-  }, 300);
-  isMenuOpen = false;
+    hiddenMenuBg.style.opacity = 0;
+    hiddenMenu.style.opacity = 0;
+    setTimeout(() => {
+        hiddenMenuBg.style.display = "none";
+        hiddenMenu.style.display = "none";
+    }, 300);
+    isMenuOpen = false;
 }
 
-btn.addEventListener('click', () => {
-  if (!isMenuOpen) {
-    openMenu();
-  } else {
+btn.addEventListener("click", () => {
+    if (!isMenuOpen) {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+});
+
+anchorMenu.addEventListener("click", () => {
     closeMenu();
-  }
+});
+anchorMenu1.addEventListener("click", () => {
+    closeMenu();
+});
+anchorMenu2.addEventListener("click", () => {
+    closeMenu();
 });
 
-anchorMenu.addEventListener('click', () => {
-  closeMenu();
-});
-anchorMenu1.addEventListener('click', () => {
-  closeMenu();
-});
-anchorMenu2.addEventListener('click', () => {
-  closeMenu();
-});
-
-closeBtn.addEventListener('click', closeMenu);
+closeBtn.addEventListener("click", closeMenu);
 
 function handleResize() {
-  if (window.innerWidth > 768 && isMenuOpen) {
-    closeMenu();
-  }
+    if (window.innerWidth > 768 && isMenuOpen) {
+        closeMenu();
+    }
 }
 
-window.addEventListener('resize', handleResize);
-
-// Project data
+window.addEventListener("resize", handleResize);
 const projects = [
-  {
-    name: 'Tonic',
-    feature: 'CANOPY',
-    backround: 'Back End Dev',
-    year: '2015',
-    description:
+    {
+        name: "Tonic",
+        feature: "CANOPY",
+        backround: "Back End Dev",
+        year: "2015",
+        description:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    featuredImage: './images/tonic-project-desktop.svg',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: 'https://example.com/tonic',
-    sourceLink: 'https://github.com/example/tonic',
-  },
-  {
-    name: 'Multi-Post Stories',
-    feature: 'CANOPY',
-    backround: 'Back End Dev',
-    year: '2015',
-    description:
+        featuredImage: "./images/tonic-project-desktop.svg",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveLink: "https://example.com/tonic",
+        sourceLink: "https://github.com/example/tonic"
+    },
+    {
+        name: "Multi-Post Stories",
+        feature: "CANOPY",
+        backround: "Back End Dev",
+        year: "2015",
+        description:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    featuredImage: './images/multi-post-project.png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: 'https://example.com/multi-post-stories',
-    sourceLink: 'https://github.com/example/multi-post-stories',
-  },
-  {
-    name: 'Tonic Orange',
-    feature: 'CANOPY',
-    backround: 'Back End Dev',
-    year: '2015',
-    description:
+        featuredImage: "./images/multi-post-project.png",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveLink: "https://example.com/multi-post-stories",
+        sourceLink: "https://github.com/example/multi-post-stories"
+    },
+    {
+        name: "Tonic Orange",
+        feature: "CANOPY",
+        backround: "Back End Dev",
+        year: "2015",
+        description:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    featuredImage: './images/tonic-orange.png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: 'https://example.com/tonic-orange',
-    sourceLink: 'https://github.com/example/tonic-orange',
-  },
-  {
-    name: 'Professional Art Printing',
-    feature: 'CANOPY',
-    backround: 'Back End Dev',
-    year: '2015',
-    description:
+        featuredImage: "./images/tonic-orange.png",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveLink: "https://example.com/tonic-orange",
+        sourceLink: "https://github.com/example/tonic-orange"
+    },
+    {
+        name: "Professional Art Printing",
+        feature: "CANOPY",
+        backround: "Back End Dev",
+        year: "2015",
+        description:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    featuredImage: './images/prof-art-project.png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: 'https://example.com/professional-art-printing',
-    sourceLink: 'https://github.com/example/professional-art-printing',
-  },
+        featuredImage: "./images/prof-art-project.png",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveLink: "https://example.com/professional-art-printing",
+        sourceLink: "https://github.com/example/professional-art-printing"
+    }
 ];
 
-const seeProjectButtons = document.querySelectorAll('.btn');
+const portfolioSection = document.getElementById("portfolio");
 
-function openPopup() {
-  const overlay = document.querySelector('.overlay');
-  const closeModalButton = overlay.querySelector('.closeModal');
-  // Get the index of the project from the data attribute
-  const projectButtons = Array.from(document.querySelectorAll('.btn'));
+const seeProjectButtons = document.querySelectorAll(".btn");
 
-  // Get the index of the clicked button dynamically
-  const projectIndex = projectButtons.indexOf(this);
-
-  //   const projectIndex = parseInt(this.getAttribute('data-project-index'), 10);
-  const project = projects[projectIndex];
-
-  const projectName = overlay.querySelector('.project-name');
-  const projectFeature = overlay.querySelector('.project-feature');
-  const projectBackground = overlay.querySelector('.project-background');
-  const projectYear = overlay.querySelector('.project-year');
-  const projectImage = overlay.querySelector('.project-image');
-  const projectDescription = overlay.querySelector('.project-description');
-  const projectTool1 = overlay.querySelector('.project-tool1');
-  const projectTool2 = overlay.querySelector('.project-tool2');
-  const projectTool3 = overlay.querySelector('.project-tool3');
-  const projectLinkLive = overlay.querySelector('.project-linkLive');
-  const projectLinkSource = overlay.querySelector('.project-linkSource');
-
-  function closePopup() {
-    const overlay = document.querySelector('.overlay');
-    overlay.style.display = 'none';
-  }
-
-  projectName.textContent = project.name;
-  projectFeature.textContent = project.feature;
-  projectBackground.textContent = project.backround;
-  projectYear.textContent = project.year;
-  projectImage.src = project.featuredImage;
-  projectDescription.textContent = project.description;
-  [projectTool1, projectTool2, projectTool3].forEach((tool, index) => {
-    if (index < project.technologies.length) {
-      tool.textContent = project.technologies[index];
-    } else {
-      tool.textContent = '';
-    }
-  });
-  projectLinkLive.href = project.liveLink;
-  projectLinkSource.href = project.sourceLink;
-
-  overlay.style.display = 'flex';
-
-  closeModalButton.addEventListener('click', closePopup);
-}
-seeProjectButtons.forEach((button) => {
-  button.addEventListener('click', openPopup);
-});
+// Add this line after dynamically creating the cards
+attachEventListeners();
 
 // Function to create the modal popup dynamically
 function createModal() {
-  const startingPoint = document.querySelector('.modal-popup');
-  startingPoint.insertAdjacentHTML(
-    'afterend',
-    `<div class='overlay'>
+    const startingPoint = document.querySelector(".modal-popup");
+    startingPoint.insertAdjacentHTML(
+        "afterend",
+        `<div class='overlay'>
             <section class='modal'>
                 <div class='modal-header flex'>
                     <h2 class='project-name '></h2>
@@ -205,53 +155,53 @@ function createModal() {
                     </li>
                 </ul>
             </section>
-        </div>`,
-  );
+        </div>`
+    );
 }
 
 createModal();
 
-document.getElementById('contact-form').addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent default form submission behavior
+document.getElementById("contact-form").addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
 
-  // Get the contact form element
-  const emailInput = document.getElementById('email');
-  const errorMessageSpan = document.getElementById('error-message-span');
+    // Get the contact form element
+    const emailInput = document.getElementById("email");
+    const errorMessageSpan = document.getElementById("error-message-span");
 
-  // Check if the email value is lowercase
-  if (emailInput.value !== emailInput.value.toLowerCase()) {
-    // Show error message
-    errorMessageSpan.textContent = 'Email should be in lower case.';
-    errorMessageSpan.style.display = 'block';
-    errorMessageSpan.style.color = 'red';
-    errorMessageSpan.style.fontSize = '14px';
-    errorMessageSpan.style.transform = 'none';
-    errorMessageSpan.style.marginTop = '5px';
-  } else {
-    // Hide error message
-    errorMessageSpan.style.display = 'none';
-    errorMessageSpan.style.color = '';
-    errorMessageSpan.style.fontSize = '';
-    errorMessageSpan.style.transform = '';
-    errorMessageSpan.style.marginTop = '';
+    // Check if the email value is lowercase
+    if (emailInput.value !== emailInput.value.toLowerCase()) {
+        // Show error message
+        errorMessageSpan.textContent = "Email should be in lower case.";
+        errorMessageSpan.style.display = "block";
+        errorMessageSpan.style.color = "red";
+        errorMessageSpan.style.fontSize = "14px";
+        errorMessageSpan.style.transform = "none";
+        errorMessageSpan.style.marginTop = "5px";
+    } else {
+        // Hide error message
+        errorMessageSpan.style.display = "none";
+        errorMessageSpan.style.color = "";
+        errorMessageSpan.style.fontSize = "";
+        errorMessageSpan.style.transform = "";
+        errorMessageSpan.style.marginTop = "";
 
-    // Submit the form
-    event.target.submit();
-  }
+        // Submit the form
+        event.target.submit();
+    }
 });
 
-window.addEventListener('resize', handleResize);
+window.addEventListener("resize", handleResize);
 
-window.addEventListener('scroll', () => {
-  const contactSection = document.getElementById('contact');
-  const navBar = document.querySelector('.NavBar');
+window.addEventListener("scroll", () => {
+    const contactSection = document.getElementById("contact");
+    const navBar = document.querySelector(".NavBar");
 
-  const contactSectionTop = contactSection.offsetTop;
-  const scrollPosition = window.scrollY;
+    const contactSectionTop = contactSection.offsetTop;
+    const scrollPosition = window.scrollY;
 
-  if (scrollPosition >= contactSectionTop) {
-    navBar.classList.add('white-bg');
-  } else {
-    navBar.classList.remove('white-bg');
-  }
+    if (scrollPosition >= contactSectionTop) {
+        navBar.classList.add("white-bg");
+    } else {
+        navBar.classList.remove("white-bg");
+    }
 });
